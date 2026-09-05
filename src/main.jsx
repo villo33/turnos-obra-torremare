02d3b2f3712fbcca4dc3e1f01b45e24742c345d5
villo33 <romeroovidio02@yahoo.com>
2026-09-05 12:02:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -20,13 +20,16 @@ ReactDOM.createRoot(
   </React.StrictMode>
 );
 
-// Registrar Service Worker para las notificaciones
+// Registrar Service Worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/sw.js")
       .then((registration) => {
-        console.log("Service Worker registrado:", registration);
+        console.log(
+          "Service Worker registrado:",
+          registration
+        );
       })
       .catch((error) => {
         console.error(
